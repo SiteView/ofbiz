9cefd4e812f02dede837360f62cafbcec02327bc
wang-zhong-ping <wang-zhong-ping@163.com>
2012-07-20 15:20:09 +0800

    Add Vysper and SvdbCache

---
+++ hot-deploy/SvdbCache/src/com/siteview/svdb/ofbiz/MonitorLogServices.java
@@ -0,0 +1,228 @@
+package com.siteview.svdb.ofbiz;
+
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javolution.util.FastList;
+import javolution.util.FastMap;
+
+import org.ofbiz.base.util.UtilMisc;
+import org.ofbiz.entity.Delegator;
+import org.ofbiz.entity.GenericValue;
+import org.ofbiz.entity.condition.EntityCondition;
+import org.ofbiz.entity.condition.EntityOperator;
+import org.ofbiz.entity.model.DynamicViewEntity;
+import org.ofbiz.entity.util.EntityListIterator;
+import org.ofbiz.service.DispatchContext;
+import org.ofbiz.service.ServiceUtil;
+
+import com.siteview.jsvapi.Jsvapi;
+
+/**
+ * 
+ * @author zhongping.wang
+ */
+public class MonitorLogServices {
+	public final static String module = MonitorLogServices.class.getName();
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+	private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+	/**
+	 * insert monitor logs
+	 * 
+	 * @param dctx
+	 * @param context
+	 * @return
+	 * @throws Exception
+	 */
+	public static Map<String, Object> insertLogData(DispatchContext dctx,
+			Map<String, ?> context) throws Exception {
+		Map<String, Object> response = ServiceUtil.returnSuccess();
+
+		Map<String, Map<String, String>> fmap = new FastMap<String, Map<String, String>>();
+		Map<String, String> ndata = new FastMap<String, String>();
+		StringBuilder estr = new StringBuilder();
+		String monitorId = "1.27.1.1";
+
+//		ndata.put("dowhat", "QueryReportData");
+//		ndata.put("id", monitorId);
+//		ndata.put("compress", "true");
+//		ndata.put("dstrNeed", "true");
+//		ndata.put("byCount", "50");
+		ndata.put("dowhat", "QueryInfo");
+		ndata.put("parentid", "sv_id,sv_monitortype,sv_name");
+		ndata.put("needkey", "monitor");
+		ndata.put("needtype", "default");
+
+		boolean ret = Jsvapi.getInstance().GetUnivData(fmap, ndata, estr);
+		if (!ret)
+			throw new Exception(estr.toString());
+		if (fmap.containsKey("return"))
+			fmap.remove("return");
+		System.out.println("从Svdb加载数据完毕<<<<<<<<<<<<<");
+		// 解析dstr节点
+		String mkey = "(dstr)" + monitorId;
+		Map<String, String> descValueMap = fmap.get(mkey);
+		String monitorName = descValueMap.get("MonitorName");
+		Iterator it = descValueMap.entrySet().iterator();
+		String entityIndex = sdf.format(new Date());// table index
+		Delegator delegator = dctx.getDelegator();
+		while (it.hasNext()) {
+			Entry entry = (Entry) it.next();
+			if (!entry.getKey().toString().equals("MonitorName")) {
+				String logId = delegator.getNextSeqId("OperationAttributeLog"
+						+ entityIndex);
+				Object descValue = entry.getValue();
+				String category = descValue.toString().substring(0,
+						descValue.toString().indexOf(" "));
+				category  = category.replaceAll("\\s*", "");
+				String decription = descValue.toString();
+				Timestamp logTime = str2Timestamp(entry.getKey().toString());
+				Map<String, String> fields = UtilMisc.toMap("logId", logId,
+						"operationId", monitorId, "logTime",
+						logTime, "name", monitorName, "category", category,
+						"description", decription, "measurement", decription);
+
+				GenericValue log = delegator.makeValue("OperationAttributeLog"
+						+ entityIndex, fields);
+				delegator.create(log);
+			}
+
+		}
+
+		return response;
+	}
+
+	/**
+	 * query monitor logs
+	 * 
+	 * @param dctx
+	 * @param context
+	 * @return
+	 * @throws Exception
+	 */
+	public static Map<String, Object> queryLogData(DispatchContext dctx,
+			Map<String, ?> context) throws Exception {
+		Map<String, Object> response = ServiceUtil.returnSuccess();
+
+		String tablename = (String) context.get("tablename");
+		int from = (Integer) context.get("from");
+		int to = (Integer) context.get("to");
+		String startTime = (String) context.get("starttime");
+		String endTime = (String) context.get("endtime");
+		Timestamp startdate = Timestamp.valueOf(startTime);
+		Timestamp enddate = Timestamp.valueOf(endTime);
+		List<EntityCondition> entityConditionList = FastList.newInstance();
+		entityConditionList.add(EntityCondition.makeCondition(
+				"logTime", EntityOperator.BETWEEN, UtilMisc.toList(
+						startdate, enddate)));
+
+		EntityCondition condition = EntityCondition.makeCondition(
+				entityConditionList, EntityOperator.AND);
+
+		DynamicViewEntity dve = new DynamicViewEntity();
+		Collection<String> fieldsToSelect = FastList.newInstance();
+		fieldsToSelect.add("operationId");
+		fieldsToSelect.add("name");
+		fieldsToSelect.add("logTime");
+		fieldsToSelect.add("category");
+		fieldsToSelect.add("description");
+		dve.addMemberEntity("OperLog", tablename);
+		dve.addAliasAll("OperLog", "");
+		List<String> orderBy = UtilMisc.toList("logTime");
+		Delegator delegator = dctx.getDelegator();
+		EntityListIterator resultiterator = delegator
+				.findListIteratorByCondition(dve, condition, null,
+						fieldsToSelect, orderBy, null);
+		List<GenericValue> result = resultiterator.getPartialList(from, to
+				- from);
+
+		FastList<String> datalist = FastList.newInstance();
+		for (GenericValue genericValue : result) {
+			String monitorid = genericValue.get("operationId").toString();
+			String reportCreateTime = genericValue.get("logTime")
+					.toString();
+			String reportDataValue = genericValue.get("description")
+					.toString();
+			String monitorStatus = genericValue.get("category").toString();
+			String monitorLog = monitorid + "#" + reportCreateTime + "#"
+					+ reportDataValue + "#" + monitorStatus;
+			datalist.add(monitorLog);
+		}
+		System.out.println("查询出< "+datalist.size()+" >条数据");
+		response.put("queryMap", datalist);
+		return response;
+	}
+
+	/**
+	 * 获取返回数量
+	 * 
+	 * @param nodeid
+	 * @return
+	 */
+	public int getReturnSize(String nodeid,
+			Map<String, Map<String, String>> returnMap) {
+		if (returnMap == null)
+			return 0;
+		int size = 0;
+
+		Map<String, String> rdata = returnMap.get(nodeid);
+		if (rdata != null) {
+			for (String key : rdata.keySet()) {
+				String value = rdata.get(key);
+				if (value != null && value.equals("ReturnValue"))
+					++size;
+			}
+		}
+
+		return size;
+	}
+
+	/**
+	 * 字符串转换时间戳
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static Timestamp str2Timestamp(String str) {
+		Date date = str2Date(str, DEFAULT_FORMAT);
+		return new Timestamp(date.getTime());
+	}
+
+	/**
+	 * 字符串转换成日期 如果转换格式为空，则利用默认格式进行转换操作
+	 * 
+	 * @param str
+	 *            字符串
+	 * @param format
+	 *            日期格式
+	 * @return 日期
+	 * @throws java.text.ParseException
+	 */
+	public static Date str2Date(String str, String format) {
+		if (null == str || "".equals(str)) {
+			return null;
+		}
+		// 如果没有指定字符串转换的格式，则用默认格式进行转换
+		if (null == format || "".equals(format)) {
+			format = DEFAULT_FORMAT;
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat(format);
+		Date date = null;
+		try {
+			date = sdf.parse(str);
+			return date;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ hot-deploy/SvdbCache/src/com/siteview/svdb/ofbiz/VysperGateway.java
@@ -0,0 +1,366 @@
+package com.siteview.svdb.ofbiz;
+
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javolution.util.FastList;
+
+import org.apache.vysper.mina.TCPEndpoint;
+import org.apache.vysper.storage.StorageProviderRegistry;
+import org.apache.vysper.storage.inmemory.MemoryStorageProviderRegistry;
+import org.apache.vysper.xmpp.addressing.Entity;
+import org.apache.vysper.xmpp.addressing.EntityImpl;
+import org.apache.vysper.xmpp.authorization.AccountCreationException;
+import org.apache.vysper.xmpp.authorization.AccountManagement;
+import org.apache.vysper.xmpp.modules.Module;
+import org.apache.vysper.xmpp.modules.extension.xep0049_privatedata.PrivateDataModule;
+import org.apache.vysper.xmpp.modules.extension.xep0050_adhoc_commands.AdhocCommandsModule;
+import org.apache.vysper.xmpp.modules.extension.xep0054_vcardtemp.VcardTempModule;
+import org.apache.vysper.xmpp.modules.extension.xep0077_inbandreg.InBandRegistrationModule;
+import org.apache.vysper.xmpp.modules.extension.xep0092_software_version.SoftwareVersionModule;
+import org.apache.vysper.xmpp.modules.extension.xep0119_xmppping.XmppPingModule;
+import org.apache.vysper.xmpp.modules.extension.xep0133_service_administration.ServiceAdministrationModule;
+import org.apache.vysper.xmpp.modules.extension.xep0202_entity_time.EntityTimeModule;
+import org.apache.vysper.xmpp.modules.roster.RosterException;
+import org.apache.vysper.xmpp.modules.roster.RosterItem;
+import org.apache.vysper.xmpp.modules.roster.SubscriptionType;
+import org.apache.vysper.xmpp.modules.roster.persistence.RosterManager;
+import org.apache.vysper.xmpp.server.XMPPServer;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
+import org.ofbiz.base.container.Container;
+import org.ofbiz.base.container.ContainerConfig;
+import org.ofbiz.base.container.ContainerException;
+import org.ofbiz.base.util.Debug;
+import org.ofbiz.base.util.FileUtil;
+import org.ofbiz.base.util.StringUtil;
+
+public class VysperGateway implements Container, MessageListener, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	protected String configFileLocation = null;
+	public static final String module = VysperGateway.class.getName();
+	protected static List<Chat> loggerChat = FastList.newInstance();
+
+	public static String XMPP_SERVER = "localhost";
+	public static Boolean XMPP_DEBUG = false;
+	public static Integer XMPP_SERVER_PORT = 5222;
+	public static String XMPP_USERNAME;
+	public static String XMPP_PASSWORD;
+	public static String MONITOR_LOG_USERNAMES;
+	public static String MONITOR_LOG_PASSWORDS;
+	private static List<String> MONITOR_LOG_USERNAME_LIST;
+
+	public static XMPPConnection connection;
+	public static Roster roster;
+	public static InetAddress ipaddr = null;
+
+	public void xmppLogin(String userName, String password)
+			throws XMPPException {
+		ConnectionConfiguration config = new ConnectionConfiguration(
+				XMPP_SERVER, XMPP_SERVER_PORT, XMPP_SERVER);
+		connection = new XMPPConnection(config);
+
+		// TODO: show reconnect progress
+		config.setReconnectionAllowed(true);
+
+		int i = 0;
+		while (!connection.isConnected()) {
+			try {
+				Thread.sleep(i * 2000);
+				connection.connect();
+			} catch (Exception ex) {
+				i++;
+				i = Math.min(i, 20);
+				Debug.logInfo("Connect to XMPP server(" + XMPP_SERVER
+						+ ") failed, wait for " + i + " seconds to retry.",
+						module);
+			}
+		}
+
+		roster = connection.getRoster();
+		// SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+		connection.login(userName, password);
+		Presence presence = new Presence(Presence.Type.available);
+		presence.setStatus("Running ...");
+		connection.sendPacket(presence);
+
+		loggerChat.clear();
+		if (MONITOR_LOG_USERNAME_LIST != null) {
+			for (String username : MONITOR_LOG_USERNAME_LIST) {
+				loggerChat.add(connection.getChatManager().createChat(
+						username + "@" + XMPP_SERVER, this));
+			}
+		} else {
+			Debug
+					.logError(
+							"No monitor loggers configured in ofbiz-containers.xml. Log data will NOT be inserted into database !",
+							module);
+		}
+	}
+
+	public void disconnect() {
+		connection.disconnect();
+	}
+
+	@Override
+	public void init(String[] args, String configFile)
+			throws ContainerException {
+		// TODO Auto-generated method stub
+		this.configFileLocation = configFile;
+		try {
+			ipaddr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean start() throws ContainerException {
+		// TODO Auto-generated method stub
+		final ContainerConfig.Container cfg = ContainerConfig.getContainer(
+				"vyspergateway-container", this.configFileLocation);
+
+		XMPP_SERVER = cfg.getProperty("xmpp").value;
+		if (XMPP_SERVER == null)
+			XMPP_SERVER = ipaddr.getHostName();
+
+		XMPP_DEBUG = Boolean.getBoolean(cfg.getProperty("xmpp_debug").value);
+		String server_port = cfg.getProperty("xmpp_port").value;
+		XMPP_SERVER_PORT = (server_port != null) ? Integer
+				.parseInt(server_port) : 5222;
+
+		XMPP_USERNAME = cfg.getProperty("xmpp_username").value;
+		if (XMPP_USERNAME == null)
+			XMPP_USERNAME = "erlangnode";
+
+		XMPP_PASSWORD = cfg.getProperty("xmpp_password").value;
+		if (XMPP_PASSWORD == null)
+			XMPP_PASSWORD = "ofbiz";
+
+		MONITOR_LOG_USERNAMES = cfg.getProperty("monitor_logger_usernames").value;
+		MONITOR_LOG_PASSWORDS = cfg.getProperty("monitor_logger_passwords").value;
+		MONITOR_LOG_USERNAME_LIST = StringUtil
+				.split(MONITOR_LOG_USERNAMES, ",");
+
+		// start the vysper xmpp server
+		startVysper("component://SvdbCache/config/bogus_mina_tls.cert");
+		try {
+			// turn on the enhanced debugger
+			XMPPConnection.DEBUG_ENABLED = XMPP_DEBUG;
+
+			// start the main client to receive the data into xmpp
+			xmppLogin(XMPP_USERNAME, XMPP_PASSWORD);
+
+			// starting the multiple clients to log the data into entity
+			final List<String> pwdList = StringUtil.split(
+					MONITOR_LOG_PASSWORDS, ",");
+			int i = 0;
+			for (i = 0; i < MONITOR_LOG_USERNAME_LIST.size(); i++) {
+				MonitorLoggers logger = new MonitorLoggers(XMPP_SERVER,
+						XMPP_SERVER_PORT, MONITOR_LOG_USERNAME_LIST.get(i),
+						pwdList.get(i), XMPP_USERNAME, Integer.parseInt(cfg
+								.getProperty("monitor_logger_buffer").value),
+						Integer.parseInt(cfg
+								.getProperty("monitor_logger_delay").value));
+				new Thread(logger).start();
+			}
+		} catch (XMPPException e1) {
+			Debug.logError(e1, module);
+		}
+
+		Runnable ofbizNode = new Runnable() {
+			public void run() {
+				serve(cfg);
+			}
+		};
+		new Thread(ofbizNode).start();
+		return true;
+	}
+
+	public void serve(ContainerConfig.Container cfg) {
+		// int indexLogger = 0;
+		while (true) {
+			// reconnect if lost connection to the server
+			while (!connection.isConnected()) {
+				try {
+					xmppLogin(XMPP_USERNAME, XMPP_PASSWORD);
+				} catch (Exception ex) {
+					Debug.logError(ex, module);
+				}
+			}
+
+			// indexLogger = (indexLogger == loggerChat.size()) ? 0:
+			// indexLogger;
+			// if ("LogMonitor".equalsIgnoreCase(message.getRequestType())){
+			// List<Object> list = FastList.newInstance();
+			// list.add(message.getAction());
+			// list.add(message.getOfbizParams());
+			// loggerChat.get(indexLogger).sendMessage(ObjectTransformation.OToS(list));
+			// indexLogger++;
+			// }
+			// else {
+			// Debug.logError("Unknown action type from erlang, allowed types are LogMonitor, OfbizService or UpdateMonitor.",
+			// module);
+			// };
+
+		}
+
+	}
+
+	private void startVysper(String fileName) {
+		String domain = XMPP_SERVER;
+
+		String addedModuleProperty = System.getProperty("vysper.add.module");
+		List<Module> listOfModules = null;
+		if (addedModuleProperty != null) {
+			String[] moduleClassNames = addedModuleProperty.split(",");
+			listOfModules = createModuleInstances(moduleClassNames);
+		}
+
+		// choose the storage you want to use
+		// StorageProviderRegistry providerRegistry = new
+		// JcrStorageProviderRegistry();
+		StorageProviderRegistry providerRegistry = new MemoryStorageProviderRegistry();
+
+		final Entity adminJID = EntityImpl.parseUnchecked("admin@" + domain);
+		final Entity erlangNodeJID = EntityImpl.parseUnchecked(XMPP_USERNAME
+				+ "@" + domain);
+		final AccountManagement accountManagement = (AccountManagement) providerRegistry
+				.retrieve(AccountManagement.class);
+		final RosterManager rosterManager = (RosterManager) providerRegistry
+				.retrieve(RosterManager.class);
+
+		String initialPassword = System.getProperty(
+				"vysper.admin.initial.password", "siteview");
+		if (!accountManagement.verifyAccountExists(adminJID)) {
+			try {
+				accountManagement.addUser(adminJID, initialPassword);
+				accountManagement.addUser(erlangNodeJID, initialPassword);
+
+				for (String user : MONITOR_LOG_USERNAME_LIST) {
+					final Entity userJID = EntityImpl.parseUnchecked(user + "@"
+							+ domain);
+					accountManagement.addUser(userJID, initialPassword);
+					rosterManager.addContact(erlangNodeJID, new RosterItem(
+							userJID, SubscriptionType.BOTH));
+					rosterManager.addContact(userJID, new RosterItem(
+							erlangNodeJID, SubscriptionType.BOTH));
+				}
+
+				// for (String user: DEBUG_USERNAME_LIST) {
+				// accountManagement.addUser(EntityImpl.parseUnchecked(user+ "@"
+				// + domain), initialPassword);
+				// }
+
+			} catch (AccountCreationException e) {
+				e.printStackTrace();
+			} catch (RosterException e) {
+				e.printStackTrace();
+			}
+		}
+
+		XMPPServer xmppserver = new XMPPServer(domain);
+		xmppserver.addEndpoint(new TCPEndpoint());
+		// server.addEndpoint(new StanzaSessionFactory());
+		xmppserver.setStorageProviderRegistry(providerRegistry);
+
+		try {
+			xmppserver.setTLSCertificateInfo(FileUtil.getFile(fileName),
+					"boguspw");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			xmppserver.start();
+			System.out.println("vysper server is running...");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		xmppserver.addModule(new SoftwareVersionModule());
+		xmppserver.addModule(new EntityTimeModule());
+		xmppserver.addModule(new VcardTempModule());
+		xmppserver.addModule(new XmppPingModule());
+		xmppserver.addModule(new PrivateDataModule());
+		xmppserver.addModule(new InBandRegistrationModule());
+		xmppserver.addModule(new AdhocCommandsModule());
+		final ServiceAdministrationModule serviceAdministrationModule = new ServiceAdministrationModule();
+		// unless admin user account with a secure password is added, this will
+		// be not become effective
+		serviceAdministrationModule.setAddAdminJIDs(Arrays.asList(adminJID));
+
+		xmppserver.addModule(serviceAdministrationModule);
+
+		if (listOfModules != null) {
+			for (Module module : listOfModules) {
+				xmppserver.addModule(module);
+			}
+		}
+
+	}
+
+	private static List<Module> createModuleInstances(String[] moduleClassNames) {
+		List<Module> modules = new ArrayList<Module>();
+
+		for (String moduleClassName : moduleClassNames) {
+			Class<Module> moduleClass;
+			try {
+				moduleClass = (Class<Module>) Class.forName(moduleClassName);
+			} catch (ClassCastException e) {
+				System.err.println("not a Vysper module class: "
+						+ moduleClassName);
+				continue;
+			} catch (ClassNotFoundException e) {
+				System.err.println("could not load module class "
+						+ moduleClassName);
+				continue;
+			}
+			try {
+				Module module = moduleClass.newInstance();
+				modules.add(module);
+			} catch (Exception e) {
+				System.err.println("failed to instantiate module class "
+						+ moduleClassName);
+				continue;
+			}
+		}
+		return modules;
+	}
+
+	@Override
+	public void stop() throws ContainerException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processMessage(Chat chat, Message message) {
+		// TODO Auto-generated method stub
+		if (message.getType() == Message.Type.chat) {
+			System.out.println(chat.getParticipant() + " says: "
+					+ message.getBody());
+			try {
+				chat.sendMessage(message.getBody() + " echo");
+			} catch (XMPPException ex) {
+				Debug.logError(ex.toString(), module);
+			}
+		}
+	}
+
+}
